6d827993b40aa308559ba37bf92f0ee0c5e4b1c5
Abhinav Gorantla <abhinav20016@gmail.com>
2021-05-17 22:17:08 +0530

    Fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React, { useState, Component } from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 
 export default class Navigation extends Component {
         render(){
             return(
                 <>
-                    <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+                    <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
                         <Container>
                             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                             <Navbar.Collapse id="responsive-navbar-nav">
-                                <Nav>
-                                    <Nav.Link href="/">Home</Nav.Link>
-                                    <Nav.Link href="/Login">Login</Nav.Link>
-                                    <Nav.Link href="/SignUp">SignUp</Nav.Link>
-                                    <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
-                                    <Nav.Link href="/Settings">Settings</Nav.Link>
+                            <Navbar.Brand href="#" className="mr-5">Crypto Tracker</Navbar.Brand>
+                                <Nav className="mr-auto ">
+                                    <Nav.Link className="mr-1" href="/">Home</Nav.Link>
+                                    <Nav.Link className="mr-1" href="/Login">Login</Nav.Link>
+                                    <Nav.Link className="mr-1" href="/SignUp">SignUp</Nav.Link>
+                                    <Nav.Link className="mr-1" href="/Dashboard">Dashboard</Nav.Link>
+                                    <Nav.Link className="mr-1" href="/Settings">Settings</Nav.Link>
                                 </Nav>
+                                <Button variant="info">Login</Button>{' '}
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
-
                 </>
             );
         }
